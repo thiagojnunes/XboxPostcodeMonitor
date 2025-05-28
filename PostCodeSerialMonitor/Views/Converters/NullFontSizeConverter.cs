4e3a9806bcaa8c4716de025cfa2b9376cbdb4f35
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-05-29 00:45:18 +0200

    fix: Silence the warning in NullFontSizeConverter

--- PostCodeSerialMonitor/Views/Converters/NullFontSizeConverter.cs
+++ PostCodeSerialMonitor/Views/Converters/NullFontSizeConverter.cs
@@ -6,12 +6,12 @@ namespace PostCodeSerialMonitor.Views.Converters;
 
 public class NullFontSizeConverter : IValueConverter
 {
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value == null ? 0d : parameter;
     }
 
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
