7175178fa1e72bdda58737e8fcfdc959644ec1c9
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-06-17 16:11:21 +0200

    fix: SemanticVersion, make Version getter publicly accessible

--- PostCodeSerialMonitor/Models/SemanticVersion.cs
+++ PostCodeSerialMonitor/Models/SemanticVersion.cs
@@ -2,30 +2,30 @@ namespace PostCodeSerialMonitor.Models;
 
 public class SemanticVersion
 {
-  private string _version { get; set; } = string.Empty;
+  public string Version { get; } = string.Empty;
 
   public SemanticVersion(string version)
   {
     //Ignore the 'v' at the beginning of the version string
     if (version.StartsWith("v"))
     {
-      _version = version.Substring(1);
+      Version = version.Substring(1);
     }
     else
     {
-      _version = version;
+      Version = version;
     }
   }
 
   /// Override greater-than operator for SemanticVersionUtils
   public static bool operator >(SemanticVersion left, SemanticVersion right)
   {
-    return string.Compare(left._version, right._version) > 0;
+    return string.Compare(left.Version, right.Version) > 0;
   }
 
   /// Override less-than operator for SemanticVersionUtils
   public static bool operator <(SemanticVersion left, SemanticVersion right)
   {
-    return string.Compare(left._version, right._version) < 0;
+    return string.Compare(left.Version, right.Version) < 0;
   }
 }
