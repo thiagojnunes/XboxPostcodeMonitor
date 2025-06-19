6d8b93686a2bccb058e3780991ff422d86f05a10
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-06-20 00:25:22 +0200

    refactor: Make SemanticVersion work more reliably

--- PostCodeSerialMonitor.Tests/SemanticVersionTests.cs
+++ PostCodeSerialMonitor.Tests/SemanticVersionTests.cs
@@ -10,12 +10,13 @@ public class SemanticVersionTestDataGenerator : IEnumerable<object[]>
         new object[] {"0.2.0.0", "0.2.10.0", true},
         new object[] {"0.2.0.0", "0.2.0.1", true},
         new object[] {"0.2.0.0", "0.3.0", true},
+        new object[] {"0.2.0.0", "0.1.99", false},
         new object[] {"0.2.0.0", "0.1.33.0", false},
         new object[] {"0.3.0", "0.3.1", true},
         new object[] {"0.3.1.0", "0.3.11.0", true},
         new object[] {"0.3.2.0", "0.3.20.0", true},
         new object[] {"0.3.11.0", "0.3.1.0", false},
-        
+
         new object[] {"1.0.0", "1.0.1", true},
         new object[] {"1.0.0", "1.1.0", true},
         new object[] {"1.0.0", "2.0.0", true},
@@ -28,7 +29,7 @@ public class SemanticVersionTestDataGenerator : IEnumerable<object[]>
         new object[] {"0.0.1", "0.0.2", true},
         new object[] {"0.0.1", "0.1.0", true},
         new object[] {"0.0.1", "1.0.0", true},
-        
+
         new object[] {"v0.2.0.0", "v0.2.1.0", true},
         new object[] {"v0.2.0.0", "0.2.1.0", true},
         new object[] {"0.2.0.0", "v0.2.1.0", true},
@@ -41,27 +42,27 @@ public class SemanticVersionTestDataGenerator : IEnumerable<object[]>
         new object[] {"v10.20.30", "v10.20.31", true},
         new object[] {"v10.20.30", "10.20.31", true},
         new object[] {"10.20.30", "v10.20.31", true},
-        
+
         new object[] {"v0.0.1", "v0.0.2", true},
         new object[] {"v0.0.1", "0.0.2", true},
         new object[] {"0.0.1", "v0.0.2", true},
         new object[] {"v1.2.3", "v1.2.3", false},
         new object[] {"v1.2.3", "1.2.3", false},
         new object[] {"1.2.3", "v1.2.3", false},
-        
+
         new object[] {"0.1.0", "v0.2.0", true},
         new object[] {"v0.1.0", "0.2.0", true},
         new object[] {"0.1.0.0", "v0.2.0", true},
         new object[] {"v0.1.0.0", "0.2.0", true},
         new object[] {"1.0.0.0", "v1.1.0", true},
         new object[] {"v1.0.0.0", "1.1.0", true},
-        
+
         new object[] {"0.9.9.9", "v1.0.0.0", true},
         new object[] {"v0.9.9.9", "1.0.0.0", true},
         new object[] {"1.99.99.99", "v2.0.0.0", true},
         new object[] {"v1.99.99.99", "2.0.0.0", true},
         new object[] {"0.0.0.1", "v0.0.0.2", true},
-        new object[] {"v0.0.0.1", "0.0.0.2", true},
+        new object[] {"v0.0.0.1", "0.0.0", false},
     };
 
     public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
@@ -71,6 +72,15 @@ public class SemanticVersionTestDataGenerator : IEnumerable<object[]>
 
 public class SemanticVersionTests
 {
+    [Fact]
+    public void TestParsing()
+    {
+        Assert.Equal([0, 1, 2, 3], new SemanticVersion("v0.1.2.3").VersionParts);
+        Assert.Equal([0, 1, 2, 3], new SemanticVersion("0.1.2.3").VersionParts);
+        Assert.Equal([0, 1, 2, 0], new SemanticVersion("v0.1.2").VersionParts);
+        Assert.Equal([0, 1, 2, 0], new SemanticVersion("0.1.2").VersionParts);
+    }
+
     [Theory]
     [ClassData(typeof(SemanticVersionTestDataGenerator))]
     public void TestComparison(string localVersion, string remoteVersion, bool isNewer)
--- PostCodeSerialMonitor/Models/SemanticVersion.cs
+++ PostCodeSerialMonitor/Models/SemanticVersion.cs
@@ -1,11 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace PostCodeSerialMonitor.Models;
 
-public class SemanticVersion
+public partial class SemanticVersion
 {
+  public const int MIN_PARTS = 3;
+  public const int EXPECTED_PARTS = 4;
   public string Version { get; } = string.Empty;
+
+  // Format: Major.Minor.Patch.<optional build>
+  public List<int> VersionParts { get; } = [];
+
+  [GeneratedRegex(@"^(?:v)?(\d+)\.(\d+)\.(\d+)\.?(\d+)?", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+  private static partial Regex VersionPartsRegex();
 
   public SemanticVersion(string version)
   {
+    var match = VersionPartsRegex().Match(version);
+
+    VersionParts = match.Groups.Values
+      // Skip first entry (the full matched string!)
+      .Skip(1)
+      .Select(x => x.Success ? int.Parse(x.Value) : 0)
+      .ToList();
+
+    if (VersionParts.Count < MIN_PARTS)
+    {
+      throw new InvalidDataException("Expecting at least 3 numbers in SemanticVersion");
+    }
+
     //Ignore the 'v' at the beginning of the version string
     if (version.StartsWith("v"))
     {
@@ -17,15 +44,25 @@ public class SemanticVersion
     }
   }
 
-  /// Override greater-than operator for SemanticVersionUtils
+  /// Override greater-than operator for SemanticVersion
   public static bool operator >(SemanticVersion left, SemanticVersion right)
   {
-    return string.Compare(left.Version, right.Version) > 0;
+    for (int i = 0; i < EXPECTED_PARTS; i++)
+    {
+      if (left.VersionParts[i] > right.VersionParts[i]) return true;
+      if (left.VersionParts[i] < right.VersionParts[i]) return false;
+    }
+    return false;
   }
 
-  /// Override less-than operator for SemanticVersionUtils
+  /// Override less-than operator for SemanticVersion
   public static bool operator <(SemanticVersion left, SemanticVersion right)
   {
-    return string.Compare(left.Version, right.Version) < 0;
+    for (int i = 0; i < EXPECTED_PARTS; i++)
+    {
+      if (left.VersionParts[i] < right.VersionParts[i]) return true;
+      if (left.VersionParts[i] > right.VersionParts[i]) return false;
+    }
+    return false;
   }
 }
