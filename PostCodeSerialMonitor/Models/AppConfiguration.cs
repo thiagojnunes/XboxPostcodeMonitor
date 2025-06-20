10392e9f9680dd8d85dacd68d68410d5c1a8d1a0
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-06-20 02:08:21 +0200

    feat: Trim config.json file (remove App/Fw Update URLs)

--- PostCodeSerialMonitor/Models/AppConfiguration.cs
+++ PostCodeSerialMonitor/Models/AppConfiguration.cs
@@ -7,7 +7,7 @@ public class AppConfiguration
 {
     [Required]
     [Range(1, int.MaxValue)]
-    public int FormatVersion { get; set; } = 1;
+    public int FormatVersion { get; set; } = 2;
 
     public bool CheckForAppUpdates { get; set; } = true;
     public bool CheckForCodeUpdates { get; set; } = true;
@@ -15,15 +15,7 @@ public class AppConfiguration
 
     [Required]
     [Url]
-    public Uri AppUpdateUrl { get; set; } = new Uri("https://example.com/todo");
-
-    [Required]
-    [Url]
     public Uri CodesMetaBaseUrl { get; set; } = new Uri("https://errors.xboxresearch.com");
-
-    [Required]
-    [Url]
-    public Uri FwUpdateUrl { get; set; } = new Uri("https://example.com/todo");
 
     [Required]
     public string Language { get; set; } = "en-US";
